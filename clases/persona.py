1a53e86ac6342e4f97dfa648291ad26317e5ddba
melybogarin <meliu91@gmail.com>
2019-07-16 23:46:35 +0200

    agregamos metodos a la clase

--- clases/persona.py
+++ clases/persona.py
@@ -1,16 +1,28 @@
-class Persona :
-    edad = 0
+class Persona : #definimos la clase , un respuesta para crear un objeto
+    edad = 0     # o tambien la clase es la "plantilla"# atributo o clase del objeto 
 
-    def __init__(self,un_nombre):
-        self.mi_nombre = un_nombre
+    def __init__(self,un_nombre): #__init__ es el metdodo constructor
+                                   #usamos self para referirnos a un objeto de si mismo 
+        self.mi_nombre = un_nombre #metodos ---- funciones dentro de la clase 
         print("hola naci,me llamo ",self.mi_nombre)
-    def cumple(self):
-        self.edad = self. edad + 1
+    def cumple(self):   # cumple esmetodo de la clse persona 
+        self.edad = self. edad + 1 # que aumenta la propiedad edad en 1 
+    def asignar_edad (self,una_edad):# asignar edad es metodo que recibe un argumento numerico 
+        self.edad = una_edad    # y asigna a la propiedad edad del objeto 
 
-pepe = Persona("pepito")
-pepa = Persona("pepita")
-print(pepe.edad)
-print(pepe. mi_nombre)
+    def asignar_nacionalidad (self,una_nacionalidad):
+        self.nacionalidad = una_nacionalidad 
+    
+     
+    def saludar (self):
+        print( "hola soy",self.mi_nombre,"y mi nacionalidad es",self.nacionalidad)
+    
+pepe = Persona ("pepito")
+pepe.asignar_nacionalidad ("pyo")
+pepe.saludar()
 
-pepe.cumple()
-print(pepe.edad)
+# ejercicio . agregar un metodo a la clse persona que asigne una nacionalidad y otro 
+# metodo que imprima "hola soy ----- y mi nacionalidad es ----""
+
+
+
